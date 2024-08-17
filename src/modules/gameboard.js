3ab4c4ac898d2907ebb0f37b0e18d412f4f58ae5
João Correia <joaocorreiaprof@gmail.com>
2024-08-17 20:04:14 +0100

    Added function to place ships into the board

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,4 +1,23 @@
 class Gameboard {
-  constructor() {}
+  constructor() {
+    this.grid = Array(10)
+      .fill(null)
+      .map(() => Array(10).fill(null));
+  }
+  placeShip(ship, startPosition, direction) {
+    const [x, y] = startPosition;
+    if (direction === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        this.grid[x][y + i] = ship;
+      }
+    }
+    if (direction === "vertical") {
+      for (let i = 0; i < ship.length; i++) {
+        this.grid[x + i][y] = ship;
+      }
+    }
+  }
+
+  receiveAttack(coordinates) {}
 }
 export { Gameboard };
